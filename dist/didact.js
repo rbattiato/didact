d088e1495a181c81a1d5df510ee6218c842ebb4f
rbattiato <rbattiato@outlook.it>
2023-02-07 11:08:51 +0100

    feat: react hello world

---
+++ dist/didact.js
@@ -0,0 +1,6 @@
+console.log('ciao');
+const element = /*#__PURE__*/React.createElement("h1", {
+  title: "foo"
+}, "Hello");
+const container = document.getElementById('root');
+ReactDOM.render(element, container);
