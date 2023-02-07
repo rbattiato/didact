c3813e575824bba20e7121e0a1d355b70a9fc4d4
rbattiato <rbattiato@outlook.it>
2023-02-07 12:02:05 +0100

    feat: element creation

--- dist/didact.js
+++ dist/didact.js
@@ -1,6 +1,55 @@
-console.log('ciao');
-const element = /*#__PURE__*/React.createElement("h1", {
-  title: "foo"
-}, "Hello");
-const container = document.getElementById('root');
-ReactDOM.render(element, container);
+/**
+ * A special Didact element which wraps all of
+ * the primite types
+ */
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: []
+    }
+  };
+}
+
+/* In its most basic form, a React element
+  is an object containing "type" and "props"
+  properties */
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child => typeof child === "object" ? child : createTextElement(child))
+    }
+  };
+}
+function render(element, container) {
+  // 1. Create DOM node
+  const dom = element.type === "TEXT_ELEMENT" ? document.createTextNode("") : document.createElement(element.type);
+
+  // 2. Add element props as node properties
+  const isProperty = key => key !== "children";
+  Object.keys(element.props).filter(isProperty).forEach(name => {
+    dom[name] = element.props[name];
+  });
+
+  // 3. Render children
+  element.props.children.forEach(child => render(child, dom));
+
+  // 4. Append element to DOM node
+  container.appendChild(dom);
+}
+const Didact = {
+  createElement,
+  render
+};
+
+/** @jsx Didact.createElement */
+const element = Didact.createElement("div", {
+  style: "background: #6f6f6f"
+}, Didact.createElement("h1", null, "Hello World"), Didact.createElement("h2", {
+  style: "text-align:right"
+}, "from Didact"));
+const container = document.getElementById("root");
+Didact.render(element, container);
--- src/didact.js
+++ src/didact.js
@@ -1,3 +1,66 @@
-const element = <h1 title="foo">Hello</h1>
-const container = document.getElementById('root')
-ReactDOM.render(element, container)
+/**
+ * A special Didact element which wraps all of
+ * the primite types
+ */
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
+/* In its most basic form, a React element
+  is an object containing "type" and "props"
+  properties */
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) =>
+        typeof child === "object" ? child : createTextElement(child)
+      ),
+    },
+  };
+}
+
+function render(element, container) {
+  // 1. Create DOM node
+  const dom =
+    element.type === "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(element.type);
+
+  // 2. Add element props as node properties
+  const isProperty = (key) => key !== "children";
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach((name) => {
+      dom[name] = element.props[name];
+    });
+
+  // 3. Render children
+  element.props.children.forEach((child) => render(child, dom));
+
+  // 4. Append element to DOM node
+  container.appendChild(dom);
+}
+
+const Didact = {
+  createElement,
+  render,
+};
+
+/** @jsx Didact.createElement */
+const element = (
+  <div style="background: #6f6f6f">
+    <h1>Hello World</h1>
+    <h2 style="text-align:right">from Didact</h2>
+  </div>
+);
+
+const container = document.getElementById("root");
+Didact.render(element, container);
